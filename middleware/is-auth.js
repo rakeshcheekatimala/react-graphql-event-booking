193bb76a99f552629f5c2943430b4fa9df384d69
RakeshCheekatimala <rakeshcheekatimala@gmail.com>
2019-04-09 18:52:04 +0800

    Adding the middleware that verifies the jwt token using verify method. Once the token is verified isAuth is set to true

---
+++ middleware/is-auth.js
@@ -0,0 +1,26 @@
+import jwt from 'jsonwebtoken';
+
+const isAuth = (req, res, next) => {
+  const token = req.signedCookies.jwt;
+
+  if (!token || token === '') {
+    req.isAuth = false;
+    return next();
+  }
+  let decodedToken;
+  try {
+    decodedToken = jwt.verify(token, process.env.secret);
+  } catch (err) {
+    req.isAuth = false;
+    return next();
+  }
+  if (!decodedToken) {
+    req.isAuth = false;
+    return next();
+  }
+  req.isAuth = true;
+  req.userId = decodedToken.userId;
+  next();
+};
+
+export default isAuth;
